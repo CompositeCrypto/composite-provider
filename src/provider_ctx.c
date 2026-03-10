1487c9e0bd33e2649c4af53eb94e8cbf9109483b
Dr. Max <director@openca.org>
2026-03-10 11:15:49 -0400

    Added COMPOSITE_CTX_new() and COMPOSITE_CTX_free() functions.

--- src/provider_ctx.c
+++ src/provider_ctx.c
@@ -1,2 +1,26 @@
 #include "provider_ctx.h"
 
+COMPOSITE_CTX *COMPOSITE_PROVIDER_CTX_new(const OSSL_CORE_HANDLE *core_handle, OSSL_LIB_CTX *libctx) {
+
+    COMPOSITE_CTX *provctx = (COMPOSITE_CTX *)malloc(sizeof(COMPOSITE_CTX));
+
+    if (!provctx) {
+        return 0;
+    }
+
+    memset(provctx, 0, sizeof(*provctx));
+    provctx->libctx = OSSL_LIB_CTX_new();
+
+    if (!provctx->libctx) {
+        free(provctx);
+        return 0;
+    }
+    return provctx;
+}
+
+void COMPOSITE_PROVIDER_CTX_free(COMPOSITE_CTX *ctx) {
+    if (ctx) {
+        OSSL_LIB_CTX_free(ctx->libctx);
+        free(ctx);
+    }
+}
--- src/provider_ctx.h
+++ src/provider_ctx.h
@@ -30,14 +30,14 @@ typedef struct composite_prov_ctx_st {
  * \param libctx The library context to use.
  * \return A pointer to the new composite provider context, or NULL on error.
  */
-COMPOSITE_CTX *composite_provider_ctx_new(const OSSL_CORE_HANDLE *core_handle, OSSL_LIB_CTX *libctx);
+COMPOSITE_CTX *COMPOSITE_PROVIDER_CTX_new(const OSSL_CORE_HANDLE *core_handle, OSSL_LIB_CTX *libctx);
 
 /*!
  * \brief Free a composite provider context.
  *
  * \param ctx The composite provider context to free.
  */
-void composite_provider_ctx_free(COMPOSITE_CTX *ctx);
+void COMPOSITE_PROVIDER_CTX_free(COMPOSITE_CTX *ctx);
 
 END_C_DECLS
 
